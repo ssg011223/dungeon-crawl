e74c05c5a176d1de148a2eda755ba053413ea389
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-02-16 12:20:39 +0100

    Add: Player inventory

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -1,8 +1,13 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.items.Item;
+
+import java.util.HashMap;
 
 public class Player extends Actor {
+    private HashMap<Item, Integer> inventory;
+
     public Player(Cell cell) {
         super(cell);
     }
@@ -10,4 +15,10 @@ public class Player extends Actor {
     public String getTileName() {
         return "player";
     }
+
+    public HashMap<Item, Integer> getInventory() { return inventory; }
+
+    public void setInventory(HashMap<Item, Integer> inventory) { this.inventory = inventory; }
+
+    public void addToInventory(Item item, Integer amount) { this.inventory.put(item, amount); }
 }
