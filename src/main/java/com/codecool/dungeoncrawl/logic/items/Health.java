37630c9893df7ef47d26dbbc490535ac5cd62647
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-02-18 10:00:12 +0100

    Add: Health class, setHealth for Actor

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Health.java
@@ -0,0 +1,20 @@
+package com.codecool.dungeoncrawl.logic.items;
+
+import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.actors.Player;
+
+public class Health extends Item{
+    public Health(Cell cell) {
+        super(cell);
+    }
+
+    @Override
+    public String getTileName() {
+        return "health";
+    }
+
+    @Override
+    public void pickUp(Player player) {
+        player.setHealth(player.getHealth() + 5);
+    }
+}
