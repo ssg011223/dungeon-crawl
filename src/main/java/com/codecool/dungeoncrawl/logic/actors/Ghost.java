16bc2867f4a877a0a7e0b4a8d31d325e1cf4c413
blippard <blippard@gmail.com>
2021-02-18 11:16:08 +0100

    Edit: ghost overrides move to move through walls

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
@@ -1,6 +1,7 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.CellType;
 
 public class Ghost extends Actor{
     public Ghost(Cell cell) {
@@ -9,7 +10,25 @@ public class Ghost extends Actor{
 
     @Override
     public String getTileName() {
-        return "ghost";
+        if (isAlive()) return "ghost";
+        return getCell().getTileName();
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        Cell nextCell;
+        try {
+            nextCell = getCell().getNeighbor(dx * 2, dy * 2);
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            nextCell = getCell();
+        }
+        if (getHiddenOccupant() == null) getCell().setActor(null);
+        else if (getHiddenOccupant().isAlive()) getCell().setActor(null);
+        else getCell().setActor(getHiddenOccupant());
+        setHiddenOccupant(nextCell.getActor());
+        nextCell.setActor(this);
+        setCell(nextCell);
+
     }
 }
 
