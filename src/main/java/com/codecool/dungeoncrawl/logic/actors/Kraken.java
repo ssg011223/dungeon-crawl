9ba976c2278bc697b09b0ede52c2f7614a4fdd08
blippard <blippard@gmail.com>
2021-02-18 10:50:55 +0100

    Add: new class created for Kraken

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Kraken.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Kraken.java
@@ -1,2 +1,15 @@
-package com.codecool.dungeoncrawl.logic.actors;public class Kraken {
+package com.codecool.dungeoncrawl.logic.actors;
+
+import com.codecool.dungeoncrawl.logic.Cell;
+
+public class Kraken extends Actor{
+
+    public Kraken(Cell cell) {
+        super(cell);
+    }
+
+    @Override
+    public String getTileName() {
+        return "kraken";
+    }
 }
