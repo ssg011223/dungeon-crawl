7e85b321b5bfe020f8be2f69e3b25bbacb5eb6c1
blippard <blippard@gmail.com>
2021-02-18 13:29:13 +0100

    Add: ghost movement through walls

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
@@ -16,6 +16,24 @@ public class Ghost extends Actor{
         return getCell().getTileName();
     }
 
+    @Override
+    public void move(int dx, int dy) {
+        Cell nextCell;
+        try {
+            nextCell = getCell().getNeighbor(dx, dy);
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            nextCell = getCell();
+        }
+        if (nextCell == null) return;
+        if (nextCell.getActor() != null && nextCell.getActor().isAlive()) return;
+        if (nextCell.getObstacle() != null) return;
+        if (getHiddenOccupant() == null) getCell().setActor(null);
+        else if (getHiddenOccupant().isAlive()) getCell().setActor(null);
+        else getCell().setActor(getHiddenOccupant());
+        setHiddenOccupant(nextCell.getActor());
+        nextCell.setActor(this);
+        setCell(nextCell);
+    }
 }
 
 
