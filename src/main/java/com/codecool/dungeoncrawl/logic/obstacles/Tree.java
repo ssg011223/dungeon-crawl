9f02c20252d7423957a56afbb6cee4989a57b6e6
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-02-18 11:06:22 +0100

    Add: Tree is removed if Player has Axe

--- src/main/java/com/codecool/dungeoncrawl/logic/obstacles/Tree.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/obstacles/Tree.java
@@ -2,6 +2,7 @@ package com.codecool.dungeoncrawl.logic.obstacles;
 
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.actors.Player;
+import com.codecool.dungeoncrawl.logic.items.Axe;
 import com.codecool.dungeoncrawl.logic.items.Item;
 
 public class Tree extends Obstacle{
@@ -16,5 +17,11 @@ public class Tree extends Obstacle{
 
     @Override
     public void remove(Player player) {
+        for (Item item: player.getInventory()) {
+            if (item instanceof Axe) {
+                this.getCell().setObstacle(null);
+                return;
+            }
+        }
     }
 }
