56a1e719bd83382f89656a0458e53ba732a14df3
blippard <blippard@gmail.com>
2021-03-04 15:15:00 +0100

    Add: unit test for save overwrite in DB

--- src/test/java/com/codecool/dungeoncrawl/logic/DataBaseTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/DataBaseTest.java
@@ -1,4 +1,47 @@
 package com.codecool.dungeoncrawl.logic;
 
+import com.codecool.dungeoncrawl.dao.GameDatabaseManager;
+import com.codecool.dungeoncrawl.logic.actors.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class DataBaseTest {
+    GameDatabaseManager gameDbManager = new GameDatabaseManager();
+    GameMap gameMap = new GameMap(3,3,CellType.FLOOR);
+    GameMap gameMap2 = new GameMap(2,2,CellType.WATER);
+    GameMap[] allMaps = {gameMap, gameMap2};
+
+    @BeforeEach
+    void createDbConnection() throws SQLException {
+        gameDbManager.setup();
+    }
+
+    @Test
+    void overWriteSave_SameNameFileAdded_OverWritesSave() {
+        Player player = new Player(gameMap.getCell(1,1));
+        gameDbManager.saveGameState(gameMap, allMaps, player, "TestSave");
+
+        assertTrue(gameDbManager.checkName("TestSave"));
+
+        gameDbManager.overWriteSave(gameMap, allMaps, player, "TestSave");
+
+        assertTrue(gameDbManager.checkName("TestSave"));
+    }
+
+    @Test
+    void overWriteSave_SaveNameIncorrect_ThrowsError() {
+        Player player = new Player(gameMap.getCell(1,1));
+        gameDbManager.saveGameState(gameMap, allMaps, player, "TestSave");
+
+        assertTrue(gameDbManager.checkName("TestSave"));
+
+        gameDbManager.overWriteSave(gameMap, allMaps, player, "TrialSaveWithDifferentName");
+
+        assertTrue(gameDbManager.checkName("TrialSaveWithDifferentName"));
+    }
+
 }
